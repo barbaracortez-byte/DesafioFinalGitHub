b38db2b946b1beda80a65f1578d7d978ab31e646
barbaracortez-byte <barbara.cortez@edu.senai.br>
2026-06-16 10:32:06 -0300

    feat: adiciona funcao situacao_final na branch melhoria
    Melhoria

--- sistema_escola_v3.py
+++ sistema_escola_v3.py
@@ -101,7 +101,13 @@ def calcular_frequencia(aulas_dadas, faltas):
     return round(percentual, 2)
 
 # >> FUNÇÃO 12: situacao_final(media, frequencia)
-
+def situacao_final(media, frequencia):
+    if media >= 6 and frequencia >= 75:
+        return "✅ Aprovado"
+    elif frequencia < 75:
+        return "❌ Reprovado por falta"
+    else:
+        return "❌ Reprovado por nota"
 
 # ------------------------------------------------------------
 # ÁREA DE TESTES — atualize conforme adiciona funções
